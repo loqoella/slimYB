c17dad4642285a1000ca24996d6b9475edaf3fc6
Rene <reneguo@outlook.com>
2019-10-15 17:33:31 +1100

    Controller initialisation.

---
+++ src/main/java/usyd/elec5619/slimYB/ProgressController.java
@@ -0,0 +1,29 @@
+package usyd.elec5619.slimYB;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class ProgressController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProgressController.class);
+
+    @RequestMapping(value = "/goal", method = RequestMethod.GET)
+    public String progress(Model model) {
+
+        model.addAttribute("title", "ProgressPage");
+
+        return "progressViews/progress";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/progressViews/progress.jsp
@@ -0,0 +1,15 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: rene
+  Date: 15/10/19
+  Time: 14:25
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ include file="/WEB-INF/views/header.jsp" %>
+
+<div class="jumbotron">
+    <h1 class="display-4">Progress</h1>
+    <p class="lead">Track your progress and goals!</p>
+</div>
+
+<%@ include file="/WEB-INF/views/footer.jsp" %>
