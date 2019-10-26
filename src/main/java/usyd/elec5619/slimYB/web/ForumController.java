8c6d90ed1317bcf3f2c3a458239ee09c6bf07bc3
loqoella <qin775251410@gmail.com>
2019-10-27 02:38:32 +1100

    1

--- src/main/java/usyd/elec5619/slimYB/web/ForumController.java
+++ src/main/java/usyd/elec5619/slimYB/web/ForumController.java
@@ -2,14 +2,23 @@ package usyd.elec5619.slimYB.web;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+
+import javax.annotation.Resource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import usyd.elec5619.slimYB.domain.Forum;
+import usyd.elec5619.slimYB.service.ForumManager;
 
 /**
  * Handles requests for the application marketplace page.
@@ -25,6 +34,9 @@ public class ForumController {
 	 * @param model - jsp context
 	 * @return
 	 */
+	@Resource(name="forumManager")
+	private ForumManager forumManager;
+	
 	@RequestMapping(value = "/forum", method = RequestMethod.GET)
 	public String home(Model model) {
 		
@@ -39,5 +51,15 @@ public class ForumController {
 		
 		return "adminSystem/forumhome";
 	}
+	
+//	@RequestMapping(value = "/view", method = RequestMethod.GET)
+//    @ResponseBody
+//    public List<Forum> view(){
+//
+//        System.out.println("view username:");
+//
+//        return forumManager.getAllForums();
+//    }
+
 	
 }
