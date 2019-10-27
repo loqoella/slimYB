a8fc1dd23fec4a26e0d9b4756505e882c8894908
mmmmxy <56291431+mmmmxy@users.noreply.github.com>
2019-10-27 22:30:55 +1100

    Merge branch 'origin/MA/profile' of https://github.com/NiftyMule/slimYB into origin/MA/profile

--- src/main/java/usyd/elec5619/slimYB/web/LoginController.java
+++ src/main/java/usyd/elec5619/slimYB/web/LoginController.java
@@ -47,7 +47,7 @@ private static final Logger logger = LoggerFactory.getLogger(LoginController.cla
 		if(password_validation.equals(userPwd)) {
 			request.getSession(true).setAttribute("currUser", user);			
 			if(email.equals("admin"))
-				return "admin";
+				return "redirect:/admin";
 			return "redirect:/";
 		}
 		else {
